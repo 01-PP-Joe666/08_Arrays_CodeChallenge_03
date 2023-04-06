ef37380e84f8781e57540fe3e5de5f600fc5ebd0
Joe123456 <maloja71mx.de>
2023-04-06 14:56:51 +0200

    neue Join

--- script_01.js
+++ script_01.js
@@ -1,4 +1,3 @@
-
 /*** Mini-Challenge  */
 // Satzbau + Punktuation
 
@@ -9,18 +8,59 @@
 
 // Parameter --> funktion getSentence([ ...], "Q"){ ...}
 
-
 /*** Funktion mit Array als Parameter */
 // mögliche Tests:
-output(getSentence(["Ich","bin","Max"],"S"));
-output(getSentence(["Bist","du","Max"],"Q"));
-output(getSentence(["Ich","bin"],"E"));
-output(getSentence(["Ich","hätt","gern","die","Platt","von","dene","zwei","diwodaso","Spass","mache,","habbe","Sie","die"],"Q"));
 
+// output(getSentence(["Ich","hätt","gern","die","Platt","von","dene","zwei","diwodaso","Spass","mache,","habbe","Sie","die"],"Q"));function getSentence(arr) {
 
+    // const GAP = " ";
+    // const PUNCT = ".";
+    // let str = "";
+    
+    // for (let i = 0; i < arr.length; i++) {
+    // if (i == arr.length-1) {
+    // str += arr[i] + PUNCT;
+    
+    // } else {
+    
+    // str += arr[i] + GAP; 
+    
+    //  }
+    
+    // }
+    
+    // return str;
+    
+    // }
 
-// Modul: Ausgabe in Konsole : Test
-// output("hi"); 
-function output(inputStr) {
-    console.log(inputStr);
-}
+    function getSentence(words, type) {
+        let sentence = words.join(" ");
+        switch (type) {
+          case "S":
+            sentence += ".";
+            break;
+          case "Q":
+            sentence += "?";
+            break;
+          case "E":
+            sentence += "!";
+            break;
+          default:
+            break;
+        }
+        return sentence;
+      }
+    
+      
+    output(getSentence(["Bist","du","Max"],"Q"));
+    output(getSentence(["Ich","bin","Max"],"S"));
+    output(getSentence(["Ich","bin"],"E"));
+    output(getSentence(["Ich","hätt","gern","die","Platt","von","dene","zwei","diwodaso","Spass","mache,","habbe","Sie","die"],"Q"));
+    
+    
+    // Modul: Ausgabe in Konsole : Test
+    // output("hi"); 
+    function output(inputStr) {
+        console.log(inputStr);
+    }
+    
